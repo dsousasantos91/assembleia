63ed26b290e434d6b1af268a5a7c20bb88833a43
Douglas Santos <>
2023-07-17 12:15:35 -0300

    implementar contagem de votos

---
+++ src/main/java/br/com/dsousasantos91/assembleia/service/dto/response/ContagemVotosResponse.java
@@ -0,0 +1,20 @@
+package br.com.dsousasantos91.assembleia.service.dto.response;
+
+import br.com.dsousasantos91.assembleia.domain.enumeration.Voto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContagemVotosResponse {
+    private PautaResponse pauta;
+    private Map<Voto, Long> votos;
+}
